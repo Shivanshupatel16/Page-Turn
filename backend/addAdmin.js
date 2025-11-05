dc93d52064871cb260bafe5a7d37a9b1e3e22b47
Shivanshu Patel <shivanshupatel2004@gmail.com>
2025-11-05 15:44:18 +0530

    fixed code for deployment

--- backend/addAdmin.js
+++ backend/addAdmin.js
@@ -1,5 +1,5 @@
 import User from "./models/User.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs";
 import connectmongo from "./database/db.js";
 import dotenv from "dotenv";
 dotenv.config(); 
--- backend/controllers/userController/loginController.js
+++ backend/controllers/userController/loginController.js
@@ -1,5 +1,5 @@
 import User from '../../models/User.js'
-import bcrypt from 'bcrypt'
+import bcrypt from "bcryptjs";
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken'
 
