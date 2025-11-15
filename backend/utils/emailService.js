5160eb35765da3d1db24c5e74f06eeb2fe5cf182
Shivanshu Patel <shivanshupatel2004@gmail.com>
2025-11-16 00:03:10 +0530

    corrected code of email

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -3,15 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: 'Gmail', 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS  
+  }
 });
-
 
 transporter.verify((error) => {
   if (error) {
