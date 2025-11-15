0add24a3f3dde4b069a14a34af8bde1eacee5e40
Shivanshu Patel <shivanshupatel2004@gmail.com>
2025-11-16 00:39:09 +0530

    changes in code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // 👈 makes "@" point to src folder
+      '@': path.resolve(__dirname, './src'), 
     },
   },
   server: {
