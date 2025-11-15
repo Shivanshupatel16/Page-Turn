6ea0b6c5ee5c6be0a8bc7484226623dc06d57441
Shivanshu Patel <shivanshupatel2004@gmail.com>
2025-11-16 01:59:16 +0530

    changes in code

--- frontend/src/buyPages/PaymentSuccessfull.jsx
+++ frontend/src/buyPages/PaymentSuccessfull.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useState , useLocation, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import {
   CheckCircleIcon,
